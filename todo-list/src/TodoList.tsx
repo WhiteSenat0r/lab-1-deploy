ad49e836a15ae9db7bd11b1137a6c8360b471379
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2025-03-18 20:29:42 +0200

    Initial commit

---
+++ todo-list/src/TodoList.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export const TodoList = () => {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [task, setTask] = useState<string>("");
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
+  const removeTask = (index: number) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h1>Todo List Cool</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>
+            {t} <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
